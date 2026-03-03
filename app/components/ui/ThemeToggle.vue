880b3d3ce9b8addf6fbb6652a8531879da78017e
hopewithoute <anggi.wibiyanto@gmail.com>
2026-03-04 04:11:38 +0700

    feat: add ThemeToggle component

---
+++ app/components/ui/ThemeToggle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const toggleTheme = () => {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+
+defineExpose({ toggleTheme })
+</script>
+
+<template>
+  <button 
+    class="hover:text-accent transition-none hover:underline underline-offset-4 decoration-2 relative group flex items-center gap-1 uppercase font-mono"
+    @click="toggleTheme"
+  >
+    <span class="opacity-0 group-hover:opacity-100 transition-opacity">[</span>
+    <span>> TOGGLE_THEME --{{ colorMode.value === 'dark' ? 'LIGHT' : 'DARK' }}</span>
+    <span class="opacity-0 group-hover:opacity-100 transition-opacity">]</span>
+  </button>
+</template>
