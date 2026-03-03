a5a66bac7c145da336fe4e69545173ec1d921d64
hopewithoute <anggi.wibiyanto@gmail.com>
2026-03-04 00:11:02 +0700

    fix: restore favicon link in nuxt head config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
+
   css: ['~/assets/css/tailwind.css'],
 
   nitro: {
