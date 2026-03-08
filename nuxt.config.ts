853f20bf23957dc99600f52a0ab4704cd729301b
hopewithoute <anggi.wibiyanto@gmail.com>
2026-03-08 20:28:05 +0700

    chore: configure global page transition in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
